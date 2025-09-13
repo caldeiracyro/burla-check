549888472d770257c0626bbdd62211802d52f621
Cyro Caldeira <53826629+caldeiracyro@users.noreply.github.com>
2025-09-13 15:06:01 -0300

    Update bulacheck.py

--- bulacheck.py
+++ bulacheck.py
@@ -1375,6 +1375,7 @@ def main():
         st.markdown("---")
         st.markdown("""
         **Desenvolvido e elaborado por Cyro Caldeira, Daniel Dourado, Gláucia Lima e Leonardo Santos**
+        
         Com a finalidade de obtenção de certificado de especialista em Ciência de Dados e Inteligência Artificial
         """)
         
@@ -1385,6 +1386,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
